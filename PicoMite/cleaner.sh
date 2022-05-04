a72e1686a78ac327add53f457df240271ec3b483
wa1tnr <wa1tnr@users.noreply.github.com>
2022-05-04 15:37:02 +0000

    prep script for new builds
    	new file:   cleaner.sh
    
    On branch dvlp-aa

---
+++ PicoMite/cleaner.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# rm -rf progname CM* el* cm* Ma* ge* pi*
+
+rm -rf ./CMakeCache.txt
+rm -rf ./CMakeFiles
+rm -rf ./Makefile
+rm -rf ./PicoMite.bin
+rm -rf ./PicoMite.dis
+rm -rf ./PicoMite.elf
+rm -rf ./PicoMite.elf.map
+rm -rf ./PicoMite.hex
+rm -rf ./PicoMite.uf2
+rm -rf ./cmake_install.cmake
+rm -rf ./elf2uf2
+rm -rf ./generated
+rm -rf ./pico-sdk
+# END.
